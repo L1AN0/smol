6df16e063aa798f6f05464d145c48f9d6cda0110
L1AN0 <57596891+L1AN0@users.noreply.github.com>
2020-05-21 03:15:58 -0700

    Update context.rs

--- src/context.rs
+++ src/context.rs
@@ -3,7 +3,6 @@
 //! Before executor, we "enter" it by setting up some necessary thread-locals.
 
 /// Enters the tokio context if the `tokio` feature is enabled.
-use std::env;
 
 pub(crate) fn enter<T>(f: impl FnOnce() -> T) -> T {
     #[cfg(not(feature = "tokio02"))]
@@ -12,7 +11,8 @@ pub(crate) fn enter<T>(f: impl FnOnce() -> T) -> T {
     #[cfg(feature = "tokio02")]
     {
         use once_cell::sync::Lazy;
-        use tokio::runtime::Builder;
+        use tokio::runtime::{Builder, Runtime};
+        use std::env;
 
         static RT: Lazy<Runtime> = Lazy::new(|| Builder::new().threaded_scheduler().core_threads(env::var("ASYNC_STD_THREAD_COUNT")
         .map(|env| {
